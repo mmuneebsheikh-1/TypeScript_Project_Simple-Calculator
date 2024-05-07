226923d8b350e216720f7172aeadac2d4961a0df
Muneeb Ashfaq <57327532+mmuneebsheikh-1@users.noreply.github.com>
2024-05-07 10:35:28 +0500

    Simple Calculator Project.
    Simple Calculator Code.

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "first_number" },
+  { message: "Enter second number", type: "number", name: "second_number" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+
+// conditional statement
+if (answer.operator === "Addition"){
+    console.log(answer.first_number + answer.second_number);
+}  else if (answer.operator === "Subtraction"){ 
+    console.log(answer.first_number - answer.second_number);
+}  else if (answer.operator === "Multiplication"){
+    console.log(answer.first_number * answer.second_number);
+}  else if (answer.operator === "Division"){
+    console.log(answer.first_number / answer.second_number);
+}  else { 
+    console.log("Please select valid operator");
+}
